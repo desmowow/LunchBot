f48ecca6b0562a28206de3a65297c4e31ffa422a
Francesco Viola <francesco.viola.external@yoox.com>
2017-12-05 12:04:33 +0100

    some refactoring

--- execute.php
+++ execute.php
@@ -20,43 +20,106 @@ $text = trim($text);
 $text = strtolower($text);
 
 
-$keyWordfirst = array("dove","andiamo","cosa","si");
-$keyWordsecond = array("mangiamo","mangiare","mangia","mangio");
-$lunchplace = array(
-  "Giga Vigliacca",
-  "Piadina",
-  "Pazzi",
-  "Focacciaro",
-  "Cerchiamo di far sorridere la Giga Vigliacca",
-  "io consiglierei il kebabbaro di fiducia",
-  "oggi una rucola e stracchino ci sta tutta eh",
-  "le vigliacche sentono la nostra mancanza",
-  "prendiamo l'autobus e adiamo al burger king",
-  "mi dispiace puma..oggi KEBAP",
-  "andiamo dai tipi strani",
-  "avete rotto...oggi decide il puma"
-);
-$random = rand(0,(count($lunchplace)-1));
+$firstArrayKeyWord = ["dove","andiamo","cosa","si"];
+$secondArrayKeyWord = ["mangiamo","mangiare","mangia","mangio"];
+$lunchPlace = [
+  new Place("Giga Vigliacca",									true,true,true,true,true,true,true),
+  new Place("Piadina",											true,true,true,true,true,true,true),
+  new Place("Pazzi",											true,true,true,true,true,true,true),
+  new Place("Focacciaro",										true,true,false,false,false,false,false),
+  new Place("Cerchiamo di far sorridere la Giga Vigliacca",		true,true,true,true,true,true,true),
+  new Place("io consiglierei il kebabbaro di fiducia",			true,true,false,false,false,false,false),
+  new Place("oggi una rucola e stracchino ci sta tutta eh",		true,true,false,false,false,false,false),
+  new Place("le vigliacche sentono la nostra mancanza",			true,true,true,true,true,true,true),
+  new Place("prendiamo l'autobus e adiamo al burger king",		true,true,true,true,true,true,true),
+  new Place("mi dispiace puma..oggi KEBAP",						true,true,false,false,false,false,false),
+  new Place("andiamo dai tipi strani",							true,true,true,true,true,true,true),
+  new Place("avete rotto...oggi decide il puma",				true,true,false,false,false,false,false),
+];
 
 
-foreach($keyWordfirst as $word1){
-  if(strpos($text, $word1) !== false){
-	  foreach($keyWordsecond as $word2){
-		  if(strpos($text, $word2) !== false){
-				header("Content-Type: application/json");
-				$parameters = array('chat_id' => $chatId, "text" => $lunchplace[$random]);
-				$parameters["method"] = "sendMessage";
-				echo json_encode($parameters);
-				exit;
-		  }
-	  }
-  }
+if(CheckKeyWord($firstArrayKeyWord, $text) && CheckKeyWord($secondArrayKeyWord, $text)){
+	$place = Place->GetRandomPlace($lunchPlace);
+	PrintJsonMessage($place->Name, $chatId);
+}
+
+function CheckKeyWord($keywords, $text){
+	foreach($keywords as $word){
+		if(strpos($text, $word) !== false){
+			return true;
+		}
+	}
+	return false;
+}
+
+function PrintJsonMessage($message, $chatId){
+	header("Content-Type: application/json");
+	$parameters = array('chat_id' => $chatId, "text" => $message);
+	$parameters["method"] = "sendMessage";
+	echo json_encode($parameters);
+}
+
+public class Place{
+	public $Name;
+	public $Monday;
+	public $Tuesday;
+	public $Wednesday;
+	public $Thursday;
+	public $Friday;
+	public $Saturday;
+	public $Sunday;
+	
+	public function __construct($name,	$monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday) {
+        $this->Name = $name;
+        $this->Monday = $monday;
+        $this->Tuesday = $tuesday;
+        $this->Wednesday = $wednesday;
+        $this->Thursday = $thursday;
+        $this->Friday = $friday;
+        $this->Saturday = $saturday;
+        $this->Sunday = $sunday;
+    }
+	
+	public function IsEnabledDay($day){
+		switch($day){
+			case 1: return $this->Monday; break;
+			case 2: return $this->Tuesday; break;
+			case 3: return $this->Wednesday; break;
+			case 4: return $this->Thursday; break;
+			case 5: return $this->Friday; break;
+			case 6: return $this->Saturday; break;
+			case 7: return $this->Sunday; break;
+		}
+	}
+	
+	public static function GetRandomPlace($places, $tryNumber=1){
+		$maxRetryNumber = 20;
+		$random = rand(0,(count($places)-1));
+		$place = $places[$random];
+		$currentDay = intval(date("N"));
+		if($place->IsEnabledDay($currentDay)){
+			return $place;
+		}else{
+			if($tryNumber < $maxRetryNumber){
+				return GetRandomPlace($places, ($tryNumber+1));
+			}else{
+				return new Place{"Uffaaaa",true,true,true,true,true,true,true);
+			}
+		}
+	}
 }
 
 
-/*
-header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
-$parameters["method"] = "sendMessage";
-echo json_encode($parameters);
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
