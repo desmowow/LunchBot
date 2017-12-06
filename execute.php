380ed7253123949b2ba44a451781eaa15edaf374
Francesco Viola <francesco.viola.external@yoox.com>
2017-12-06 12:14:34 +0100

    save place in file

--- execute.php
+++ execute.php
@@ -22,20 +22,7 @@ $text = strtolower($text);
 
 $firstArrayKeyWord = ["dove","andiamo","cosa","si"];
 $secondArrayKeyWord = ["mangiamo","mangiare","mangia","mangio"];
-$lunchPlace = [
-  new Place("Giga Vigliacca",									true,true,true,true,true,true,true),
-  new Place("Piadina",											true,true,true,true,true,true,true),
-  new Place("Pazzi",											true,true,true,true,true,true,true),
-  new Place("Focacciaro",										true,true,false,false,false,false,false),
-  new Place("Cerchiamo di far sorridere la Giga Vigliacca",		true,true,true,true,true,true,true),
-  new Place("io consiglierei il kebabbaro di fiducia",			true,true,false,false,false,false,false),
-  new Place("oggi una rucola e stracchino ci sta tutta eh",		true,true,false,false,false,false,false),
-  new Place("le vigliacche sentono la nostra mancanza",			true,true,true,true,true,true,true),
-  new Place("prendiamo l'autobus e adiamo al burger king",		true,true,true,true,true,true,true),
-  new Place("mi dispiace puma..oggi KEBAP",						true,true,false,false,false,false,false),
-  new Place("andiamo dai tipi strani",							true,true,true,true,true,true,true),
-  new Place("avete rotto...oggi decide il puma",				true,true,false,false,false,false,false),
-];
+$lunchPlace = Place::FromJSON(file_get_contents("lunchPlace.txt"));
 
 
 if(CheckKeyWord($firstArrayKeyWord, $text) && CheckKeyWord($secondArrayKeyWord, $text)){
@@ -69,15 +56,15 @@ class Place{
 	public $Saturday;
 	public $Sunday;
 	
-	public function __construct($name,	$monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday) {
-        $this->Name = $name;
-        $this->Monday = $monday;
-        $this->Tuesday = $tuesday;
-        $this->Wednesday = $wednesday;
-        $this->Thursday = $thursday;
-        $this->Friday = $friday;
-        $this->Saturday = $saturday;
-        $this->Sunday = $sunday;
+	public function __construct($mixed) {
+        $this->Name = $mixed->Name;
+        $this->Monday = $mixed->Monday;
+        $this->Tuesday = $mixed->Tuesday;
+        $this->Wednesday = $mixed->Wednesday;
+        $this->Thursday = $mixed->Thursday;
+        $this->Friday = $mixed->Friday;
+        $this->Saturday = $mixed->Saturday;
+        $this->Sunday = $mixed->Sunday;
     }
 	
 	public function IsEnabledDay($day){
@@ -107,6 +94,18 @@ class Place{
 			}
 		}
 	}
+	
+	public static FromJSON($json)
+	{
+		$array_obj = json_decode($json);
+		$result = [];
+		foreach($array_obj as $obj){
+			$result[] = new self($obj);
+		}
+		return $result;
+	}
+
+   
 }
 
 
